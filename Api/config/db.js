afe0ced7c5316ea745d636335dc2e6827157ba42
angel <angelcruz.feb@gmail.com>
2023-11-27 09:33:05 -0600

    create deployment archives #3

--- Api/config/db.js
+++ Api/config/db.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const { Pool } = require('pg');
 
 // Configura la conexión a la base de datos
 const pool = new Pool({
-  user: "postgres", // Nombre de usuario de PostgreSQL
-  host: "localhost", // Host de PostgreSQL (por lo general, 'localhost')
-  database: "dbicec2", // Nombre de la base de datos
-  password: "admin", // Contraseña de PostgreSQL
-  port: 5432, // Puerto de PostgreSQL (por lo general, 5432)
+  user: process.env.TEST_DB_USER, // Nombre de usuario de PostgreSQL
+  host: process.env.TEST_DB_HOST, // Host de PostgreSQL (por lo general, 'localhost')
+  database: process.env.TEST_DB_DATABASE, // Nombre de la base de datos
+  password: process.env.TEST_DB_PASSWORD, // Contraseña de PostgreSQL
+  port: process.env.TEST_DB_PORT, // Puerto de PostgreSQL (por lo general, 5432)
 });
 
 module.exports = pool;
---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:16.19.0
+
+WORKDIR /Api-Auth
+
+COPY package.json .
+COPY package-lock.json .
+
+RUN npm install
+
+COPY . /Api-Auth
+
+EXPOSE 3000
+
+CMD ["node", "app.js"] 
